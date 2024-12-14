187e5a7c29dd4007e20507aee9089069bc8844e5
Felipe Mendes <lfelipecmendes@icloud.com>
2024-12-14 13:46:17 -0300

    add `database.js` module

---
+++ infra/database.js
@@ -0,0 +1,27 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  await client.connect();
+
+  try {
+    const result = await client.query(queryObject);
+
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
