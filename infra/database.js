7ec0f983b43f96e24d6e369ad60fba2f4ed40998
Felipe Mendes <lfelipecmendes@icloud.com>
2024-12-15 12:37:31 -0300

    moved database connection inside the trycatch block

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,17 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("Credrentials:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
 
     return result;
